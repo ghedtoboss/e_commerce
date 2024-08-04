db0dd12664abd42695ad9f3355882640cc191b4c
ghedtoboss <sezeregemen@gmail.com>
2024-08-04 16:21:54 +0300

    update product

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -5,7 +5,10 @@ import (
 	"e_commerce/models"
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func AddProduct(w http.ResponseWriter, r *http.Request) {
@@ -35,4 +38,42 @@ func AddProduct(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(product)
+}
+
+func UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	productID, err := strconv.Atoi(params["product_id"])
+	if err != nil {
+		http.Error(w, "Invalid id.", http.StatusBadRequest)
+		return
+	}
+
+	var input models.Product
+	err = json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, "Invalid input.", http.StatusBadRequest)
+		return
+	}
+
+	var product models.Product
+	if result := database.DB.First(&product, productID); result.Error != nil {
+		http.Error(w, "Product not found.", http.StatusNotFound)
+		return
+	}
+
+	product.Stock = input.Stock
+	product.Price = input.Price
+	product.UpdatedAt = time.Now()
+	product.ImageUrl = input.ImageUrl
+	product.Description = input.Description
+	product.Name = input.Name
+
+	if result := database.DB.Save(&product); result.Error != nil {
+		http.Error(w, "Failed to update product.", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Product updated successfully."})
+
 }
